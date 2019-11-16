51143b0da62d395682464981fa4de87b41154cd4
George <george.marr.cobc@gmail.com>
2019-11-16 18:49:36 +0000

    make sure it's not being ran on windows

--- battery.h
+++ battery.h
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 #include<errno.h>
+
+#ifdef LINUX
 #include<dirent.h>
 #include<linux/limits.h>
 #include<regex.h>
+#endif 
 
 #define DATA_DIR = "sys/class/power_supply"
 
--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include "battery.h"
 
+#ifdef _WIN32
+#define WIN_IN_USE true
+#endif
+
+#ifdef LINUX 
+#define LIN_IN_USE true
+#endif
+
 int main(int argc, char **argv) {
-    while(true) {
-        display_battery();
+    if(WIN_IN_USE) {
+        fprintf("[X] You cannot run this program on windows!");
+        exit(0);
+    } else {
+        while(true) {
+            display_battery();
+        }
     }
     return 0;
 }
