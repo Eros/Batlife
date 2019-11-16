7ba0230cdb9f0ae28f07f1ca4af35c7a24aaec72
George <george.marr.cobc@gmail.com>
2019-11-16 18:35:50 +0000

    lets make this a bit cleaner

---
+++ batter.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+#include<dirent.h>
+#include<linux/limits.h>
+#include<regex.h>
+
+#define DATA_DIR = "sys/class/power_supply"
+
+void run();
