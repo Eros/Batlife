d7c5bef647f157fe51155d01e230498f9f9a72ec
eros <george.marr.cobc@gmail.com>
2019-11-16 18:13:14 +0000

    this should work

--- main.c
+++ main.c
@@ -23,5 +23,40 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Could not open dir: %s\n", sterror(errno));
         return 3;
     }
+
+    //the actual reading of the file and the battery life
+    while((dp ==  readdir(d) != NULL)) {
+        snprintf(b, PATH_MAX, "Opened dir: %s\n", strerror(errno));
+
+        //define the regex to read the correct parts of the file otherwise
+        //it will just print random crap that's not required
+        regex_t regex;
+        //esure that the data that we need is in there, otherwise it's pretty damn useless
+        if(regcomp(&regex, "BAT[[:alnum:]]+", REG_EXTENDED) != 0) {
+            fprintf(stderr, "Could not complete required regex: %s\n", sterror(errno));
+            return 4;
+        }
+
+        //this is where things get messy
+        if(regexec(&regex, b, 0, NULL, 0) == NULL) {
+            snprintf(b, PATH_MAX, "%s%s%s", DATA_DIR, dp->d_name, "charge_now");
+            fc = fopen(b, "r");
+            snprintf(b, PATH_MAX, "%s%s%s", DATA_DIR, dp->d_name, "charge_full");
+            ff = fopen(b, "r");
+            if(fc != NULL && ff != NULL) {
+                if(fscanf(fc, "%ld", &current) != 1 || fscanf(ff, "%ld", &full) != 1) {
+                    fprintf(stderr, "Could not scan required fules: %s\n", sterror(errno));
+                } else {
+                    fprintf(stdout, "Current charge: %s %f\n", dp->d_name, (current / full) * 100.0);
+                }
+                //close the files now that we're done with them
+                fclose(fc);
+                fclose(ff);
+            }   
+        }
+
+        //close the regex and free it from the memory block
+        regfree(&regex);
+    }
     return 0;
 }
