fddd363c560a5f87f19f1f736400e5a186708fba
George <george.marr.cobc@gmail.com>
2019-11-17 00:12:40 +0000

    add in mac check

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include "battery.h"
 #include<stdbool.h>
 
-bool windows;
+bool is_linux;
 
 #ifdef _WIN32
-windows = true;
+is_linux = false;
 #endif
 
 #ifdef LINUX 
-windows = false;
+is_linux = true;
+#endif
+
+#ifdef DARWIN 
+is_linux = false;
 #endif
 
 int main(int argc, char **argv) {
-    if(windows) {
+    if(!is_linux) {
         fprintf("[X] You cannot run this program on windows!", "");
         exit(0);
     } else {
